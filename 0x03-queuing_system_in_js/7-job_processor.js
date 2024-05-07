6315385556b4d080f84c0cef5242718537af9413
Shadkoech <shadrackipruto@gmail.com>
2024-05-07 17:53:28 +0300

    Track progress and errors with Kue: Create the Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,28 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+
+// Array of blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+    // Track the progress of the job
+    job.progress(0, 100);
+
+    // Check if phoneNumber is blacklisted
+    if (blacklistedNumbers.includes(phoneNumber)) {
+        return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    }
+
+    // Track the progress to 50%
+    job.progress(0, 50);
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    return done();
+  }
+  
+  queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message, job, done);
+});
