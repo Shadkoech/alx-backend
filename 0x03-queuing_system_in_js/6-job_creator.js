6100b0cfac5aed9d0ca3abc0074b54e3b285560f
Shadkoech <shadrackipruto@gmail.com>
2024-05-07 15:11:03 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,29 @@
+import { createQueue } from 'kue';
+
+// create an instance of queue
+const queue = createQueue();
+
+// Creat a job object
+const jobData = {
+    phoneNumber: '+254727221023',
+    message: 'Contact Number',
+};
+
+// Create a job and add it to the queue
+const job = queue.create('push_notification_code', jobData).save((error)=>{
+    if(error){
+        return
+    } else{
+        console.log(`Notification job created: ${job.id}`);
+    }
+});
+
+// When job is completed
+job.on('complete', ()=>{
+    console.log('Notification job completed');
+});
+
+// When job fails
+job.on('failed', ()=>{
+    console.log('Notification job failed');
+});
